ffa12eb00818380ccc5daf285e8c795022574c9b
Pablo Santiago Semhan <santiagosemhan@gmail.com>
2017-02-25 12:44:08 -0300

    bug fix

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 //secure group
-Route::group(['middleware'=>['auth:api'],function() {
+Route::group(['middleware'=>'auth:api'],function() {
 
 	Route::resource('equipos','EquipoController');
 
